f47803903c6a62de526652b39850a1598f17207a
abdelhakimkhaouiti <abdelhakimkhaouiti62@gmail.com>
2024-01-12 23:17:23 +0100

    changing the function to handle whatever number of args

--- program.py
+++ program.py
@@ -1,6 +1,14 @@
-def add(a,b):
-    return a+b
+def add(*args):
+    return sum(args)
 
-a,b = 1,2
-sum = add(a,b)
-print(sum)
+a, b, c = 1, 2, 3
+
+#trying the function with 2 args
+result1 = add(a, b)
+print(result1)
+#output: 3
+
+#trying the function with 3 args
+result2 = add(a, b, c)
+print(result2)
+#output: 6
